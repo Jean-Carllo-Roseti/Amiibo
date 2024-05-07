18961299873c2ed39e80b43affcc16800140456c
jean carllo <jeanrosete04@gmail.com>
2024-05-07 11:47:42 -0300

    necessario modificar a transição do hamburger.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,30 +1,59 @@
 import { Link } from 'react-router-dom'
-import { HContent, HList } from './styles'
+import { HContent, HList, Hitem, Hamburguer, HamLink } from './styles'
 import IconMario from '../../assets/image/iconMario.png'
+import { useState } from 'react'
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
     <HContent className="sticky-top">
       <div className="container">
         <HList>
+          <div>
+            <Hamburguer onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              <span />
+              <span />
+              <span />
+            </Hamburguer>
+            <HamLink className={isMenuOpen ? 'is-open' : ''}>
+              <ul className="p-0">
+                <li>
+                  <Link className=" link " to={'/gameseries'}>
+                    <h2>Séries</h2>
+                  </Link>
+                </li>
+                <li>
+                  <Link className=" link " to={'/page'}>
+                    <h2>Personagens</h2>
+                  </Link>
+                </li>
+                <li>
+                  <Link className=" link " to={'/type'}>
+                    <h2>Tipos</h2>
+                  </Link>
+                </li>
+              </ul>
+            </HamLink>
+          </div>
           <Link className=" link" to={'/'}>
             <img className="mario" src={IconMario} alt="Imagem do Mario" />
           </Link>
-          <li>
-            <Link className="link" to={'/gameseries'}>
+          <Hitem>
+            <Link className="link " to={'/gameseries'}>
               <h2>Séries</h2>
             </Link>
-          </li>
-          <li>
-            <Link className="link" to={'/page'}>
+          </Hitem>
+          <Hitem>
+            <Link className="link " to={'/page'}>
               <h2>Personagens</h2>
             </Link>
-          </li>
-          <li>
-            <Link className="link" to={'/type'}>
+          </Hitem>
+          <Hitem>
+            <Link className="link " to={'/type'}>
               <h2>Tipos</h2>
             </Link>
-          </li>
+          </Hitem>
         </HList>
       </div>
     </HContent>
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -20,6 +20,10 @@ export const HList = styled.ul`
   align-items: center;
   margin-bottom: 0;
 
+  @media (max-width: 768px) {
+    justify-content: space-around;
+  }
+
   img {
     width: 84px;
   }
@@ -33,6 +37,10 @@ export const HList = styled.ul`
     :hover {
       transform: translateY(-3px);
     }
+
+    @media (max-width: 768px) {
+      border-bottom: 4px solid #fff;
+    }
   }
 
   .link {
@@ -40,9 +48,44 @@ export const HList = styled.ul`
     text-decoration: none;
     transition: transform 0.5s ease;
     display: inline-block;
+
+    @media (max-width: 768px) {
+      color: #fff;
+    }
   }
 
   .link:hover {
     color: #fff;
+  }
+`
+
+export const Hamburguer = styled.div`
+  width: 32px;
+  margin-bottom: 8px;
+
+  span {
+    height: 2px;
+    display: block;
+    width: 100%;
+    background-color: #fff;
+    margin-bottom: 4px;
+
+    @media (min-width: 768px) {
+      display: none;
+    }
+  }
+`
+export const Hitem = styled.li`
+  @media (max-width: 768px) {
+    display: none;
+  }
+`
+export const HamLink = styled.div`
+  display: none;
+  transition: height 0.3s ease;
+
+  &.is-open {
+    display: block;
+    height: 120px;
   }
 `
