a158f2efef6698562c586b22a400ff20c6ed70a7
jean carllo <jeanrosete04@gmail.com>
2024-05-09 10:20:15 -0300

    ajuste carrossel.

--- src/components/carrossel/styel.ts
+++ src/components/carrossel/styel.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
 export const ImageCar = styled.img`
-  height: 600px;
-  width: 100%;
-  object-fit: cover;
+  height: 568px;
+
+  object-fit: fill;
 
   @media (max-width: 768px) {
     height: 280px;
